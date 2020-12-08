2a061b7f4376affa97b9715c9b0f56435542daf8
drakodav <vmedves@gmail.com>
2020-12-08 15:31:39 +0000

    working map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,34 +2,73 @@ import React, { FunctionComponent, useEffect } from 'react';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
+import OSM from 'ol/source/OSM';
 
 type Props = {};
 
-export const PublicMap: FunctionComponent<Props> = (Props) => {
-  const map = new Map({
-    target: 'map',
-    layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+// export const PublicMap: FunctionComponent<Props> = (Props) => {
+//   const map = new Map({
+//     target: 'my-map',
+//     layers: [
+//       new TileLayer({
+//         source: new OSM(),
+//       }),
+//     ],
+//     view: new View({
+//       center: [0, 0],
+//       zoom: 2,
+//     }),
+//   });
+//   console.log(map, 'hello');
+
+//   useEffect(() => {
+//     map.setTarget('my-map');
+//   });
+
+//   return (
+//     <div id='my-map' style={{ width: '100%', height: '500px' }}>
+//       {/* <button onClick={(e) => this.userAction()}>setState on click</button> */}
+//     </div>
+//   );
+// };
+
+export class PublicMap extends React.Component {
+  componentDidMount() {
+    // create feature layer and vector source
+    // var featuresLayer = new Vector({
+    //   source: new Vector({
+    //     features: [],
+    //   }),
+    // });
+
+    // create map object with feature layer
+    var map = new Map({
+      target: 'mapContainer',
+      layers: [
+        //default OSM layer
+        new TileLayer({
+          source: new OSM(),
         }),
+        // featuresLayer,
+      ],
+      view: new View({
+        center: [-11718716.28195593, 4869217.172379018], //Boulder, CO
+        zoom: 13,
       }),
-    ],
-    view: new View({
-      center: [0, 0],
-      zoom: 2,
-    }),
-  });
-  console.log(map, 'hello');
+    });
 
-  useEffect(() => {
-    map.setTarget('map');
-  });
+    // save map and layer references to local state
+    this.setState({
+      map: map,
+      // featuresLayer: featuresLayer,
+    });
+  }
 
-  return (
-    <div id='map' style={{ width: '100%', height: '360px' }}>
-      {/* <button onClick={(e) => this.userAction()}>setState on click</button> */}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div id='mapContainer' style={{ width: '100%', height: '500px' }}>
+        {' '}
+      </div>
+    );
+  }
+}
