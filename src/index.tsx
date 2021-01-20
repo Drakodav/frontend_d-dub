da0ffddc73afac926e26e81217bc2ae104edb684
drakodav <vmedves@gmail.com>
2021-01-20 17:57:51 +0000

    move theming logic

--- src/index.tsx
+++ src/index.tsx
@@ -6,40 +6,13 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-
-// adds window information to reducer
-
-const theme = createMuiTheme({
-    palette: {
-        common: { black: '#000', white: '#fff' },
-        background: { paper: '#fff', default: '#fafafa' },
-        primary: {
-            light: 'rgba(182, 227, 255, 1)',
-            main: 'rgba(130, 177, 255, 1)',
-            dark: 'rgba(77, 130, 203, 1)',
-            contrastText: '#fff',
-        },
-        secondary: {
-            light: 'rgba(215, 255, 217, 1)',
-            main: 'rgba(165, 214, 167, 1)',
-            dark: 'rgba(117, 164, 120, 1)',
-            contrastText: '#fff',
-        },
-        error: { light: '#e57373', main: '#f44336', dark: '#d32f2f', contrastText: '#fff' },
-        text: {
-            primary: 'rgba(0, 0, 0, 0.87)',
-            secondary: 'rgba(0, 0, 0, 0.54)',
-            disabled: 'rgba(0, 0, 0, 0.38)',
-            hint: 'rgba(0, 0, 0, 0.38)',
-        },
-    },
-});
+import { ThemeProvider } from '@material-ui/core';
+import { MyTheme } from './model/theme';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <ThemeProvider theme={theme}>
+            <ThemeProvider theme={MyTheme}>
                 <App />
             </ThemeProvider>
         </Provider>
---
+++ src/model/theme.ts
@@ -0,0 +1,27 @@
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+export const MyTheme = createMuiTheme({
+    palette: {
+        common: { black: '#000', white: '#fff' },
+        background: { paper: '#fff', default: '#fafafa' },
+        primary: {
+            light: 'rgba(182, 227, 255, 1)',
+            main: 'rgba(130, 177, 255, 1)',
+            dark: 'rgba(77, 130, 203, 1)',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: 'rgba(215, 255, 217, 1)',
+            main: 'rgba(165, 214, 167, 1)',
+            dark: 'rgba(117, 164, 120, 1)',
+            contrastText: '#fff',
+        },
+        error: { light: '#e57373', main: '#f44336', dark: '#d32f2f', contrastText: '#fff' },
+        text: {
+            primary: 'rgba(0, 0, 0, 0.87)',
+            secondary: 'rgba(0, 0, 0, 0.54)',
+            disabled: 'rgba(0, 0, 0, 0.38)',
+            hint: 'rgba(0, 0, 0, 0.38)',
+        },
+    },
+});
