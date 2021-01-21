b67885c63fc16ea8f074d67e8fe56e3430b0c773
drakodav <vmedves@gmail.com>
2021-01-21 17:35:24 +0000

    improve mobileView

--- src/components/MobileView.tsx
+++ src/components/MobileView.tsx
@@ -1,62 +1,69 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
-import { KeyboardArrowDown, KeyboardArrowUp } from '@material-ui/icons';
+import { KeyboardArrowUp } from '@material-ui/icons';
 import { ApiSearchInput } from './ApiSearchInput';
 import { ApiInputType } from '../model/api.model';
 import { updateMapHeight } from '../store/reducers/map';
 import { useDispatch } from 'react-redux';
 import { TRANSITION_DURATION } from '../model/constants';
 
-const useStyles = makeStyles(({ palette }) => ({
-    container: {
-        position: 'fixed',
-        backgroundColor: palette.common.white,
-        padding: '10px',
-        bottom: 0,
-        left: 0,
-        zIndex: 1,
-        borderRadius: '5px',
-        display: 'flex',
-        flex: 2,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        touchAction: 'none',
-    },
-    pair: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-}));
+const useStyles = (state: typeof initState) =>
+    makeStyles(({ palette }) => ({
+        container: {
+            position: 'fixed',
+            backgroundColor: palette.common.white,
+            padding: '10px',
+            bottom: 0,
+            left: 0,
+            zIndex: 1,
+            borderRadius: '10px',
+            display: 'flex',
+            flex: 2,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            touchAction: 'none',
+            height: state.height,
+            flexDirection: !state.open ? 'column' : 'column',
+            transition: `height ${state.tranistionSpeed}ms`,
+        },
+        arrow: {
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            transform: state.open ? 'rotateX(540deg)' : 'none',
+            transition: `transform ${TRANSITION_DURATION}ms`,
+        },
+    }));
 
 const heights = {
-    min: 60,
-    mid: 350,
-    max: 600,
-    step: 50,
-};
+    min: 60 as number,
+    mid: 350 as number,
+    max: 600 as number,
+    step: 50 as number,
+} as const;
 
 const initState = {
-    open: false,
-    height: heights.min,
-    prevState: heights.min,
-    prevY: 0,
-    direction: 0,
-    initY: 0,
-    diffHeight: 0,
-    tranistionSpeed: TRANSITION_DURATION,
-};
+    open: false as boolean,
+    height: heights.min as number,
+    prevState: heights.min as number,
+    prevY: 0 as number,
+    direction: 0 as number,
+    initY: 0 as number,
+    diffHeight: 0 as number,
+    tranistionSpeed: TRANSITION_DURATION as number,
+} as const;
 
 type State = typeof initState | { [key: string]: unknown };
 
 export function MobileView() {
     const dispatch = useDispatch();
-    const classes = useStyles();
     const ref = useRef<HTMLDivElement>(null);
 
     const [state, tempState] = useState(initState);
     const setState = (val: State) => tempState({ ...state, ...val });
+
+    const classes = useStyles(state)();
 
     // componentWillUnmount
     useEffect(() => {
@@ -73,21 +80,26 @@ export function MobileView() {
     };
 
     const onTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-        if (!!e && e.touches[0]) {
+        if (!!e && e.touches[0] && ref?.current === e.target) {
             const curr = e.touches[0].clientY;
             if (!state.prevY) {
                 // there needs to be a prev Y value in order to calculate direction
                 setState({ prevY: curr, tranistionSpeed: 0 });
                 return;
             }
+            let prevState = state.prevState;
             let open = state.height > heights.min + heights.step;
 
             const direction = curr < state.prevY ? 1 : -1;
             const newHeight = window.innerHeight - curr - state.diffHeight;
 
+            // if curr touch passes through middle then we can reach the further breakpoint
+            if (newHeight > heights.mid - heights.step && newHeight < heights.mid + heights.step)
+                prevState = heights.mid;
+
             // set the new height if it is within the boundary
             if (newHeight >= heights.min - heights.step && newHeight <= heights.max + heights.step)
-                setState({ height: newHeight, prevY: curr, direction, open });
+                setState({ height: newHeight, prevY: curr, direction, open, prevState });
         }
     };
 
@@ -138,19 +150,8 @@ export function MobileView() {
             onTouchEnd={onTouchEnd}
             maxWidth='sm'
             className={classes.container}
-            style={{
-                height: state.height,
-                flexDirection: !state.open ? 'row' : 'column',
-                transition: `height ${state.tranistionSpeed}ms`,
-            }}
         >
-            <div className={classes.pair}>
-                {!state.open ? (
-                    <KeyboardArrowUp onClick={openCloseMenu} />
-                ) : (
-                    <KeyboardArrowDown onClick={openCloseMenu} />
-                )}
-            </div>
+            <KeyboardArrowUp onClick={openCloseMenu} className={classes.arrow} />
             <ApiSearchInput disabled={!state.open} heading={'Bus Route'} query={ApiInputType.route} />
             <ApiSearchInput disabled={!state.open} heading={'Bus Stop'} query={ApiInputType.stop} />
         </Container>
